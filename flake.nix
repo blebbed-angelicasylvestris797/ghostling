d577f515a6f3cbec28f44fe55a74a9d8d81e8227
Mitchell Hashimoto <m@mitchellh.com>
2026-03-20 14:29:19 -0700

    ci: try to fix

--- flake.nix
+++ flake.nix
@@ -27,6 +27,15 @@
             pkgs.ninja
             pkgs.pinact
             pkgs.scc
+          ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+            pkgs.xorg.libX11
+            pkgs.xorg.libXcursor
+            pkgs.xorg.libXrandr
+            pkgs.xorg.libXinerama
+            pkgs.xorg.libXi
+            pkgs.libGL
+            pkgs.libxkbcommon
+            pkgs.wayland
           ];
 
           # Unset Nix Darwin SDK env vars and remove the xcbuild
